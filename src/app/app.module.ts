883d137dcce5cbdd9c3541e55344d503afdfbd13
siddharthsachdeva2835 <siddharthsachdeva2835@gmail.com>
2018-10-10 17:19:36 +0530

    all components and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NavComponent } from './nav/nav.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
+
+// const routes: Routes = {
+//   {path: 'signup', component:  },
+//   {path: 'signup', component:  },
+//   {path: 'signup', component:  }
+// };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent,
+    ChatroomComponent,
+    FeedComponent,
+    MessageComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    NavComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
